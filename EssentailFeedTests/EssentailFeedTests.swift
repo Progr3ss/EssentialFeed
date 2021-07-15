187d13d1407ff22b437c94f0ce33a0a39198447c
Martin <mchibwe01@gmail.com>
2021-07-15 16:13:12 -0700

    Delivers connectivity	error on client error

--- EssentailFeedTests/EssentailFeedTests.swift
+++ EssentailFeedTests/EssentailFeedTests.swift
@@ -11,35 +11,41 @@ import XCTest
 class RemoteFeedLoaderTests: XCTestCase {
     func test_init_doesNotRequestDataFromURL() {
         let (_, client) = makeSUT()
-        XCTAssertNil(client.requestedURL)
+        XCTAssertTrue(client.requestedURLs.isEmpty)
     }
     func test_load_requestDataFromURL() {
         let url = URL(string: "https://a-rul.com")!
         let (sut,client) = makeSUT(url: url)
         sut.load()
-        XCTAssertEqual(client.requestedURL, url)
+        XCTAssertEqual(client.requestedURLs, [url])
+    }
+    func test_load_deliverErrorOnClientError() {
+        let (sut, client) = makeSUT()
+        client.error = NSError(domain: "Test", code: 0)
+        var capturedError: RemoteFeedLoader.Error?
+        sut.load{ error in capturedError = error}
+        
+        XCTAssertEqual(capturedError, .connectivity)
     }
     func test_loadTwice_requestsDataFromURLTwice() {
         let url = URL(string: "https://a-rul.com")!
         let (sut,client) = makeSUT(url: url)
         sut.load()
-        sut.load()
-        XCTAssertEqual(client.requestedURLs, [url,url])
-        XCTAssertEqual(client.requestedURL, url)
+        XCTAssertEqual(client.requestedURLs, [url])
     }
     private func makeSUT(url: URL =  URL(string: "asdfasdf")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
         let sut =  RemoteFeedLoader(url: url, client: client)
         return (sut, client)
     }
-   private  class HTTPClientSpy: HTTPClient {
-       var requestedURL: URL?
+    private  class HTTPClientSpy: HTTPClient {
        var requestedURLs = [URL]()
-    
-       func get(from url: URL){
-            requestedURL = url
-        requestedURLs.append(url)
+       var error: Error?
+        func get(from url: URL, completion: @escaping (Error) -> Void) {
+            if let error = error {
+                completion(error)
+            }
+            requestedURLs.append(url)
         }
-        
     }
 }
