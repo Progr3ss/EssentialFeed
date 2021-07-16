17d312c29e98c775ea64e84bb91b8c1178c67789
Martin <mchibwe01@gmail.com>
2021-07-15 17:10:03 -0700

    combine spy captured

--- EssentailFeedTests/EssentailFeedTests.swift
+++ EssentailFeedTests/EssentailFeedTests.swift
@@ -13,41 +13,46 @@ class RemoteFeedLoaderTests: XCTestCase {
         let (_, client) = makeSUT()
         XCTAssertTrue(client.requestedURLs.isEmpty)
     }
+    
     func test_load_requestDataFromURL() {
         let url = URL(string: "https://a-rul.com")!
         let (sut,client) = makeSUT(url: url)
         sut.load()
         XCTAssertEqual(client.requestedURLs, [url])
     }
+    
     func test_load_deliverErrorOnClientError() {
         let (sut, client) = makeSUT()
         var capturedError = [RemoteFeedLoader.Error]()
         sut.load{capturedError.append($0)}
         let clientError = NSError(domain: "Test", code: 0)
-//        client.completions[0](clientError)
         client.complete(with: clientError)
         XCTAssertEqual(capturedError, [.connectivity])
     }
+    
     func test_loadTwice_requestsDataFromURLTwice() {
         let url = URL(string: "https://a-rul.com")!
         let (sut,client) = makeSUT(url: url)
         sut.load()
         XCTAssertEqual(client.requestedURLs, [url])
     }
+    
     private func makeSUT(url: URL =  URL(string: "asdfasdf")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
         let sut =  RemoteFeedLoader(url: url, client: client)
         return (sut, client)
     }
+    
     private  class HTTPClientSpy: HTTPClient {
-       var requestedURLs = [URL]()
-       var completions = [(Error) -> Void]()
+        private var messages = [(url: URL, completion: (Error) -> Void)]()
+        var requestedURLs:[URL]{
+            return messages.map{$0.url}
+        }
         func get(from url: URL, completion: @escaping (Error) -> Void) {
-            completions.append(completion)
-            requestedURLs.append(url)
+            messages.append((url, completion))
         }
         func complete(with error: Error, at index: Int = 0) {
-            completions[index](error)
+            messages[index].completion(error)
         }
     }
 }
