494266d809e6b36f8462ba531abfa3874b7475a7
Martin <mchibwe01@gmail.com>
2021-07-15 17:00:57 -0700

    improve readability with helper method

--- EssentailFeedTests/EssentailFeedTests.swift
+++ EssentailFeedTests/EssentailFeedTests.swift
@@ -24,7 +24,8 @@ class RemoteFeedLoaderTests: XCTestCase {
         var capturedError = [RemoteFeedLoader.Error]()
         sut.load{capturedError.append($0)}
         let clientError = NSError(domain: "Test", code: 0)
-        client.completions[0](clientError)
+//        client.completions[0](clientError)
+        client.complete(with: clientError)
         XCTAssertEqual(capturedError, [.connectivity])
     }
     func test_loadTwice_requestsDataFromURLTwice() {
@@ -44,6 +45,9 @@ class RemoteFeedLoaderTests: XCTestCase {
         func get(from url: URL, completion: @escaping (Error) -> Void) {
             completions.append(completion)
             requestedURLs.append(url)
+        }
+        func complete(with error: Error, at index: Int = 0) {
+            completions[index](error)
         }
     }
 }
