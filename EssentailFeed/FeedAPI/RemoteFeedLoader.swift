cb9f98f85e85675ca7fac706bef85cdc686d8b77
Martin <mchibwe01@gmail.com>
2021-07-18 15:12:20 -0700

    Delivers items array on 200 HTTP response with JSON items

--- EssentailFeed/FeedAPI/RemoteFeedLoader.swift
+++ EssentailFeed/FeedAPI/RemoteFeedLoader.swift
@@ -33,8 +33,8 @@ public class RemoteFeedLoader {
         client.get(from: url){ result   in
             switch result {
             case let  .success(data, _ ):
-                if (try?  JSONSerialization.jsonObject(with: data)) != nil {
-                    completion(.success([]))
+                if let root = try? JSONDecoder().decode(Root.self, from: data) {
+                    completion(.success(root.items))
                 }else  {
                     completion(.failure(.invalidData))
                 }
@@ -43,4 +43,8 @@ public class RemoteFeedLoader {
             }
         }
     }
+}
+
+private struct Root: Decodable {
+    let items: [FeedItem]
 }
